839b526d50824d054c161bb6ff2ce808a74247a0
Syzygianinfern0 <spsharan2000@gmail.com>
2022-03-03 02:21:48 +0530

    Some logging

--- bin/install_dots.sh
+++ bin/install_dots.sh
@@ -3,22 +3,24 @@
 # Part 0: Back up oh my zsh stuff so it doesnt complain
 mkdir -p .dotfiles-backup
 if [ -d .oh-my-zsh ]; then
-   mv .oh-my-zsh/ .dotfiles-backup/
+  echo "Backing up oh my zsh folder."
+  mv .oh-my-zsh/ .dotfiles-backup/
 fi
 
 # Part 1: Get the oh my zsh things working
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone -qq --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone -qq https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone -qq https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+git clone -qq https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone -qq https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 
 # Part 2: Get the dotfiles in place
 if [ -d .dotfiles ]; then
-   mv .dotfiles .dotfiles-backup
+  echo "Backing up .dotfiles folder."
+  mv .dotfiles .dotfiles-backup
 fi
-git clone --bare https://github.com/Syzygianinfern0/dotfiles.git $HOME/.dotfiles
+git clone -qq --bare https://github.com/Syzygianinfern0/dotfiles.git $HOME/.dotfiles
 function config {
   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
